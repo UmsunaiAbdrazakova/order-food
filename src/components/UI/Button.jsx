ca340d32d5ac080b21faea9c7c7c9d69909c615a
Umsunai <aubkg1011@gmail.com>
2023-08-01 23:28:16 +0600

    add cart logic

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,83 +1,105 @@
-import React from "react";
-import styled, { css } from "styled-components";
+import React from 'react'
+import { css, styled } from 'styled-components'
 
-const StyledButton = styled.button`
-  display: flex;
-  border: none;
-  outline: none;
-  padding: 0.5rem 1.25rem;
-  border-radius: 20px;
-  cursor: pointer;
-  ${({ variant }) => variantStyles[variant]};
-  ${({ size }) => sizeStyles[size]};
-`;
+const BUTTON_VARIANTS = {
+	outlined: {
+		bgColor: 'transparent',
+		color: '#8a2b06',
+		bgColorOnHover: '#8a2b06',
+		bgColorOnActive: '#993108',
+		disabled: {
+			bgColor: 'transparent',
+			color: '#cac6c4',
+		},
+	},
+	contained: {
+		bgColor: '#8a2b06',
+		color: '#fff',
+		bgColorOnHover: '#7e2a0a',
+		bgColorOnActive: '#993108',
+		disabled: {
+			bgColor: '#cac6c4',
+			color: '#fff',
+		},
+	},
+}
 
-const variantStyles = {
-  outlined: css`
-    border: 1px solid #8a2b06;
-    background-color: transparent;
-    color: #8a2b06;
-    &:hover:not(:disabled) {
-      background-color: #8a2b06;
-      color: #ffffff;
-    }
-    &:active:not(:disabled) {
-      background-color: #993108;
-    }
-    &:disabled {
-      color: #cac6c4;
-      background-color: transparent;
-      border: 1px solid #cac6c4;
-    }
-  `,
-  contained: css`
-    background-color: #8a2b06;
-    color: #ffffff;
-    &:hover:not(:disabled) {
-      background: #7e2a0a;
-    }
-    &:active:not(:disabled) {
-      background-color: #993108;
-    }
-    &:disabled {
-      color: #ffffff;
-      background: #cac6c4;
-    }
-  `,
-};
+const getStylesByVariant = (variant) => {
+	const currentVariant = BUTTON_VARIANTS[variant]
+	return css`
+		background-color: ${currentVariant.bgColor};
+		color: ${currentVariant.color};
+		&:hover {
+			background-color: ${currentVariant.bgColorOnHover};
+		}
+		&:active {
+			background-color: ${currentVariant.bgColorOnActive};
+		}
+		&:disabled {
+			background-color: ${currentVariant.disabled.bgColor};
+			color: ${currentVariant.disabled.color};
+		}
+	`
+}
 
-const sizeStyles = {
-  small: css`
-    font-size: 14px;
-  `,
-  medium: css`
-    font-size: 16px;
-  `,
-  large: css`
-    font-size: 20px;
-  `,
-  default: css`
-    font-size: 16px;
-  `,
-};
+const BUTTON_SIZES = {
+	small: '14px',
+	medium: '16px',
+	large: '18px',
+}
 
 export const Button = ({
-  children,
-  onClick,
-  disabled,
-  variant,
-  size = "default",
-  ...restProps
+	children,
+	onClick,
+	disabled,
+	variant = 'contained',
+	size = 'medium',
+	IconComponent = null,
+	type = 'submit',
+	...restProps
 }) => {
-  return (
-    <StyledButton
-      variant={variant}
-      onClick={onClick}
-      disabled={disabled}
-      size={size}
-      {...restProps}
-    >
-      {children}
-    </StyledButton>
-  );
-};
+	const isHasIcon = Boolean(IconComponent)
+	return (
+		<StyledButton
+			onClick={onClick}
+			disabled={disabled}
+			variant={variant}
+			icon={String(isHasIcon)}
+			size={size}
+			{...restProps}
+		>
+			{isHasIcon && <IconComponent />}
+			{children}
+		</StyledButton>
+	)
+}
+
+const StyledButton = styled('button')`
+	outline: none;
+	cursor: pointer;
+	padding: 0.625rem 2rem;
+	border-radius: 20px;
+	border: none;
+	font-weight: 500;
+	border: 1px solid #8a2b06;
+	color: white;
+	&:hover {
+		color: white;
+	}
+	&:disabled {
+		border: 1px solid #cac6c4;
+		cursor: not-allowed;
+	}
+	transition: all 0.1s ease-out;
+	${(props) => getStylesByVariant(props.variant || 'contained')}
+	${(props) => {
+		if (props.icon === 'false') return null
+		return css`
+			display: flex;
+			align-items: center;
+			gap: 10px;
+			font-weight: 700;
+		`
+	}};
+	font-size: ${(props) => BUTTON_SIZES[props.size || 'medium']};
+`
